593606ef0e1f068b9b281c04c733fd8fa0e4596c
arfin <md.arfinshekh2017@gmail.com>
2019-04-24 18:56:56 +0600

    project

---
+++ JSF/JSF-with-XML/src/java/controller/StudentController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import pojo.Student;
+
+/**
+ *
+ * @author Asaduzzaman Rony
+ */
+public class StudentController {
+
+    private Student student;
+
+    public Student getStudent() {
+        if(student == null){
+            student = new Student();
+        }
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+}
